3a3320c39b99e3a58594999cb5fd823d4d9b12ec
Bulat_laptop <blubsanoff777@gmail.com>
2024-04-06 17:28:05 +0800

    First commit

---
+++ src/components/MyHello.vue
@@ -0,0 +1,27 @@
+<!-- Options API -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script> -->
+
+<!-- Composition APi -->
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+
+function increment() {
+  count.value++
+  console.log(count)
+}
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="increment">+</button>
+</template>
